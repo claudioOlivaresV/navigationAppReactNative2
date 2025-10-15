54c436849a25bc99cb8fc2f4e11c100f4ed110d6
Claudio Olivares <ca.olivares91@gmail.com>
2025-10-15 17:19:00 -0300

    first commit

---
+++ Documents/proyectos/componentesReactNative/src/ComponentsApp.tsx
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+
+import {Navigator} from './presentation/navigator/Navigator';
+import {ThemeProvider} from './presentation/context/ThemeContext';
+import { Text } from 'react-native';
+
+export const ComponentsApp = () => {
+  return <Text>Hola Mundo</Text>;
+};
